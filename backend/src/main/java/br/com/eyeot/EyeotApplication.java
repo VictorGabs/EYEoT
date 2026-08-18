5217416af9a164061d627dab851de1b65579dfe0
Victor Gabriel <victorgabriel4123@gmail.com>
2026-08-18 17:20:37 -0300

    Comitando estrutura inicial do projeto.

---
+++ backend/src/main/java/br/com/eyeot/EyeotApplication.java
@@ -0,0 +1,13 @@
+package br.com.eyeot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyeotApplication.class, args);
+	}
+
+}
